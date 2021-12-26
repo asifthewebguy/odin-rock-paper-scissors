4bb1be34cabcdd5befa252bc6d355dec94a108e6
Mohammad Asif Ullah Chowdhury <asifthewebguy@gmail.com>
2021-12-27 04:59:08 +0600

    script.js file updated

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("Hello World");
+let yourScore = 0;
+let computerScore = 0;
+let rounds = 5;
+
+function computerPlay(){
+    var options = ['Rock', 'Paper', 'Scissors'];
+    var random = Math.floor(Math.random() * 3);
+    return options[random];
+}
+
+function playRound(playerSelection, computerSelection){
+    ps = playerSelection.toLowerCase();
+    cs = computerSelection.toLowerCase();
+    console.log(ps, cs);
+    if (ps == "rock" && cs == "scissors") {
+        yourScore +=1;
+        return "You Win!, Rock beats Scissors!";
+    }else if(ps == "rock" && cs == "paper") {
+        computerScore +=1;
+        return "You Lose! Paper beats Rock";
+    }else{
+        rounds += 1;
+        return "It's a tie! Play again.";
+    }
+}
+
+
+function game(){
+    let score = 0;
+    for (let i = 0; i < rounds; i++) {
+        const playerSelection = window.prompt("message", "rock");
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("your score: " + yourScore + " & computer's score: " + computerScore);
+    }
+}
+game();
